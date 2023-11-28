b2555a07ccbecd02cd664eab37b7a20b6d7cdc57
Simon Egger <st160727@stud.uni-stuttgart.de>
2023-11-28 09:14:01 +0100

    Simplify complete_flip.h
    Instead of continuously maintaining a candidate_map when a new edge is
    examined (via boost::examine_edge), we now only store the predecessor
    and backtrack the cycle when boost::back_edge is reached.

--- src/dgm/complete_flip.h
+++ src/dgm/complete_flip.h
@@ -1,67 +1,74 @@
 #ifndef TSN_DGM_COMPLETE_FLIP_H
 #define TSN_DGM_COMPLETE_FLIP_H
 
-#include "shuffle_graph.h"
-#include "traversal.h"
+#include "critical_path.h"
 
 namespace tsndgm {
 
-template <typename Graph>
-class complete_flip_visitor : public boost::default_dfs_visitor {
+class complete_flip_visitor : public longest_path_visitor {
 public:
-  typedef boost::graph_traits<Graph>::vertex_descriptor Vertex;
-  typedef boost::graph_traits<Graph>::edge_descriptor Edge;
+  complete_flip_visitor(
+      shuffle_graph_t &shuffle_graph,
+      std::set<boost::OrientationState *> &flipped_edges,
+      std::set<boost::OrientationState *> &required_flips_classes,
+      std::list<E> &required_flips)
+      : longest_path_visitor(shuffle_graph), flipped_edges(flipped_edges),
+        required_flips_classes(required_flips_classes),
+        required_flips(required_flips) {}
 
-  enum State {
-    not_visited_flipped_edge,
-    visited_flipped_edge,
-    visited_flipped_edge_first
-  };
-
-  complete_flip_visitor(const std::set<OrientationStatePair *> &flipped_edges,
-                        std::list<Edge> &required_flips)
-      : flipped_edges(flipped_edges), required_flips(required_flips) {}
-
-  void back_edge(Edge e, const Graph &g) {
-    auto search = candidate_map.find(source(e, g));
-    if (search == candidate_map.end())
-      throw std::runtime_error(
-          "Cyclic route for message stream with handle " +
-          std::to_string(g[source(e, g)].ms_handle.front()));
-    if (search->second.second == not_visited_flipped_edge)
-      throw std::runtime_error(
-          "Shuffle graph contained cycle before complete flip");
-    else if (search->second.second == visited_flipped_edge)
-      required_flips.push_back(search->second.first);
-    else
-      required_flips.push_back(e);
+  void tree_edge(E e, const shuffle_graph_t &shuffle_graph) const {
+    prop.cycle_pred[source(e, shuffle_graph)] = target(e, shuffle_graph);
   }
 
-  void examine_edge(Edge e, const Graph &g) {
-    auto search = candidate_map.find(source(e, g));
-    if (search != candidate_map.end()) {
-      if (search->second.second != not_visited_flipped_edge ||
-          g[e].edge_type == conjunctive)
-        candidate_map[target(e, g)] = search->second;
-      else if (flipped_edges.contains(g[e].state_pair.get()))
-        candidate_map[target(e, g)] =
-            std::make_pair(search->second.first, visited_flipped_edge);
-      else
-        candidate_map[target(e, g)] =
-            std::make_pair(e, not_visited_flipped_edge);
+  void back_edge(E uv, const shuffle_graph_t &shuffle_graph) const {
+    // uv may be flipped by previously called back_edge
+    if (required_flips_classes.contains(
+            shuffle_graph[uv].state_pair->state.get()))
+      return;
+
+    V u = source(uv, shuffle_graph), v = target(uv, shuffle_graph), w = v;
+    bool flipped_uv =
+        flipped_edges.contains(shuffle_graph[uv].state_pair->state.get());
+    std::pair<E, bool> prev = std::make_pair(uv, flipped_uv);
+
+    std::optional<E> candidate = {};
+    while (w != u) {
+      E e = boost::edge(w, prop.cycle_pred[w], shuffle_graph).first;
+      w = prop.cycle_pred[w];
+
+      // conjunctive edges cannot be flipped, continue
+      if (shuffle_graph[e].edge_type == conjunctive)
+        continue;
+
+      // check if cycle is already eliminated by previously called back_edge
+      if (required_flips_classes.contains(
+              shuffle_graph[e].state_pair->state.get()))
+        return;
+
+      if (!flipped_edges.contains(shuffle_graph[e].state_pair->state.get()))
+        prev = std::make_pair(e, false);
+      else if (!prev.second)
+        candidate = prev.first;
+    }
+    if (flipped_uv && !prev.second)
+      candidate = prev.first;
+
+    if (candidate.has_value()) {
+      E e = candidate.value();
+      required_flips.push_back(e);
+      required_flips_classes.insert(shuffle_graph[e].state_pair->state.get());
+    } else if (prev.second) {
+      // todo: can this case ever occur?
+      throw std::runtime_error(
+          "Found cycle where every disjunctive edge was already flipped");
     } else {
-      if (flipped_edges.contains(g[e].state_pair.get()))
-        candidate_map[target(e, g)] =
-            std::make_pair(e, visited_flipped_edge_first);
-      else if (g[search->second.first].edge_type != conjunctive)
-        candidate_map[target(e, g)] =
-            std::make_pair(e, not_visited_flipped_edge);
+      throw std::runtime_error("Shuffle graph was not acyclic before flip");
     }
   }
 
-  const std::set<OrientationStatePair *> &flipped_edges;
-  std::map<Vertex, std::pair<Edge, State>> candidate_map;
-  std::list<Edge> &required_flips;
+  std::set<boost::OrientationState *> &flipped_edges;
+  std::set<boost::OrientationState *> &required_flips_classes;
+  std::list<E> &required_flips;
 };
 
 } // namespace tsndgm
