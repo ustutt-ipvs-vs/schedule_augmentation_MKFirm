a05edb1233cb2ce23faa92b67327844372482728
Heiko Geppert <heiko.geppert@ipvs.uni-stuttgart.de>
2024-06-03 16:54:41 +0200

    Update cmake to fetch nlohmann_json, add generic executable, update gitignore

---
+++ main.cpp
@@ -0,0 +1,22 @@
+//
+// Created by gepperho on 03.06.2024.
+//
+
+
+auto main(int argc, char* argv[])
+    -> int
+{
+    /*
+     * TODO:
+     * - get command line arguments (CLI11?)
+     * - parse network file
+     * - parse streams file
+     * - parse schedule file
+     *
+     * - build dgm graph from schedule
+     * - update dgm edge weights
+     * - check dgm graph for feasibility
+     *
+     * - create output
+     */
+}
