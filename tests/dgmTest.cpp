9db557b892c387a0767308b943c6b781df3f9177
Thinkbook 14s <robin.laidig@ipvs.uni-stuttgart.de>
2024-06-14 10:23:42 +0200

    Implemented dgm tests for all conjunctive edge weights from outgoing src edges and incoming sink edges.

--- tests/dgmTest.cpp
+++ tests/dgmTest.cpp
@@ -14,8 +14,6 @@ protected:
   dgmTest() : dgm(network, streams, scheduled_streams) {
     // Code in this constructor will be executed before *each* test
 
-    // For debugging, this should print the dgm before each test, if it is initialized correctly
-    dgm.print();
   }
 
   // Runs *once* before the tests to build the dgm transmission graph
@@ -41,3 +39,60 @@ std::vector<tsndgm::StreamSchedule> dgmTest::scheduled_streams;
 TEST_F(dgmTest, operationVerticesTest) {}
 
 TEST_F(dgmTest, conjunctiveEdgesTest) {}
+
+TEST_F(dgmTest, weightEdgesFromSrcTest) {
+    auto src = dgm.transmission_graph[boost::graph_bundle].src;
+    using boost::make_iterator_range;
+
+    for (auto current_edge : make_iterator_range(out_edges(src, dgm.transmission_graph))){
+      auto targetV = dgm.transmission_graph[boost::target (current_edge, dgm.transmission_graph)];
+
+      if(targetV.stream_id == 0 && targetV.frame_number == 0){
+        ASSERT_EQ(dgm.transmission_graph[current_edge].weight, 17400);
+      }
+      else if(targetV.stream_id == 0 && targetV.frame_number == 1){
+        ASSERT_EQ(dgm.transmission_graph[current_edge].weight, 117400);
+      }
+      else if(targetV.stream_id == 1 && targetV.frame_number == 0){
+        ASSERT_EQ(dgm.transmission_graph[current_edge].weight, 38840);
+      }
+      else if(targetV.stream_id == 2 && targetV.frame_number == 0){
+        ASSERT_EQ(dgm.transmission_graph[current_edge].weight, 200);
+      }
+      else if(targetV.stream_id == 2 && targetV.frame_number == 1){
+        ASSERT_EQ(dgm.transmission_graph[current_edge].weight, 100200);
+      }
+      else{
+        FAIL() << "Found edge from src that does not match any stream and frame number! Stream_id: " << targetV.stream_id << ", Frame_number: " << targetV.frame_number;
+      }
+    }
+}
+
+TEST_F(dgmTest, weightEdgesToSinkTest) {
+    auto sink = dgm.transmission_graph[boost::graph_bundle].sink;
+    using boost::make_iterator_range;
+
+    for (auto current_edge : make_iterator_range(in_edges(sink, dgm.transmission_graph))){
+      auto sourceV = dgm.transmission_graph[boost::source (current_edge, dgm.transmission_graph)];
+
+      if(sourceV.stream_id == 0 && sourceV.frame_number == 0){
+        ASSERT_EQ(dgm.transmission_graph[current_edge].weight, 10724);
+      }
+      else if(sourceV.stream_id == 0 && sourceV.frame_number == 1){
+        ASSERT_EQ(dgm.transmission_graph[current_edge].weight, 10724);
+      }
+      else if(sourceV.stream_id == 1 && sourceV.frame_number == 0){
+        ASSERT_EQ(dgm.transmission_graph[current_edge].weight, 10900);
+      }
+      else if(sourceV.stream_id == 2 && sourceV.frame_number == 0){
+        ASSERT_EQ(dgm.transmission_graph[current_edge].weight, 8604);
+      }
+      else if(sourceV.stream_id == 2 && sourceV.frame_number == 1){
+        ASSERT_EQ(dgm.transmission_graph[current_edge].weight, 8604);
+      }
+      else{
+        FAIL() << "Found edge to sink that does not match any stream and frame number! Stream_id: " << sourceV.stream_id << ", Frame_number: " << sourceV.frame_number;
+      }
+    }
+
+}
